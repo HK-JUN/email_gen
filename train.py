59e97908e787841a9753c1dd3ed53f9551dc2940
HK-JUN <hkjun6515@gmail.com>
2024-11-02 12:45:28 +0900

    train preprocessing edit

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import torch
 from transformers import BartTokenizer, BartForConditionalGeneration, Trainer, TrainingArguments
 from datasets import Dataset,DatasetDict
 import pandas as pd
-#need to define dataset fraction into the dataset
+#Bart version.
 train_data = pd.read_csv('/home/user3/workplace/dataset/train/combined_train.csv')
 test_data = pd.read_csv('/home/user3/workplace/dataset/test/combined_test.csv')
 train_dataset = Dataset.from_pandas(train_data)
@@ -18,7 +18,7 @@ tokenizer = BartTokenizer.from_pretrained(model_name)
 model = BartForConditionalGeneration.from_pretrained(model_name)
 
 def preprocess(batch):
-    input_text = f"politeness: {batch['p_tag']} / email content:{batch['summary']}" #fix! - not a single object. batch.
+    input_text = [f"politeness: {p_tag} / email content:{summary}" for p_tag,summary in zip(batch['p_tag'],batch['summary'])] #fix! - not a single object. batch.
     inputs = tokenizer(input_text,max_length=512,truncation=True,padding="max_length",return_tensors="pt")
     targets = tokenizer(batch['txt'],max_length=150,truncation=True,padding="max_length",return_tensors="pt")
     batch['input_ids'] = inputs['input_ids']
@@ -36,17 +36,17 @@ training_args = TrainingArguments(
     output_dir="../results",
     evaluation_strategy="epoch",
     learning_rate=2e-5,
-    per_device_training_batch_size=2,
+    per_device_train_batch_size=2,
     per_device_eval_batch_size=2,
-    num_training_epoch=3,
+    num_train_epochs=3,
     weight_decay=0.01,
     save_total_limit=3
 )
 
 trainer = Trainer(
-    model=model,
+    model = model,
     args = training_args,
-    train_dataset=train_dataset,
+    train_dataset= train_dataset,
     eval_dataset = test_dataset
 )
 
